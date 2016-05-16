7db5aacabb101006cdc94cb6ed60f630128209d9
Leonardo Pereira Macedo <leonardo.macedo@usp.br>
2016-05-16 18:16:15 -0300

    Fix 'Detail' tests that check for empty String and nil

--- app/models/detail.rb
+++ app/models/detail.rb
@@ -2,4 +2,14 @@ class Detail < ApplicationRecord
   belongs_to :event, dependent: :destroy
 
   validates :component_uuid, :event_id, :presence => true
+  validate :disallow_empty_and_nil
+
+  # Raises error if Detail has empty String or 'nil' as a value for any attribute
+  def disallow_empty_and_nil
+    if self.component_uuid.blank? || self.capability.blank? || self.data_type.blank?
+       || self.unit.blank? || self.value.blank?
+         self.errors.add :base, 'Cannot add empty String!'
+    end
+  end
+
 end
