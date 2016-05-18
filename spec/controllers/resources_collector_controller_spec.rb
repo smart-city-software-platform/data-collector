089a02d321151ec7b6063b807c9e8780f28f4d61
rodrigosiqueira <rodrigosiqueiramelo@gmail.com>
2016-05-18 17:12:22 -0300

    Added handler for new resource in data collect.

---
+++ spec/controllers/resources_collector_controller_spec.rb
@@ -0,0 +1,65 @@
+require 'rails_helper'
+
+RSpec.describe ResourcesCollectorController, :type => :controller do
+
+  subject {response}
+  let(:json) {JSON.parse(response.body)}
+
+  before :each do
+    event_params = FactoryGirl.attributes_for(:event)
+    post :create, params: {resources_collector: event_params}
+  end
+
+  it 'has a valid factory' do
+    expect(FactoryGirl.create(:event)).to be_valid
+  end
+
+  context 'verify create method' do
+
+    it 'verify request ends successfully' do
+      is_expected.to have_http_status(201)
+    end
+
+    it 'verify if request stored data' do
+      event_params = {category: 'push', resource_id: 152, date: DateTime.now}
+      expect{post :create, params: {resources_collector: event_params}}.to change(Event, :count).by(1)
+    end
+
+  end
+
+  context 'verify update method' do
+    it 'verify request ends successfully' do
+      last_event = Event.last
+      put :update, params: {id: last_event.id, resources_collector: {category: 'pull'}}
+      is_expected.to have_http_status(200)
+    end
+
+    it 'verify if request update data (check resource_id)' do
+      last_event = Event.last
+      put :update, params: {id: last_event.id, resources_collector: {resource_id: 777}}
+      last_event = Event.last
+      expect(last_event.resource_id).to eq(777)
+    end
+
+    it 'verify if request update data (check category)' do
+      last_event = Event.last
+      put :update, params: {id: last_event.id, resources_collector: {category: 'pull'}}
+      last_event = Event.last
+      expect(last_event.category).to eq('pull')
+    end
+
+    it 'verify if request update data (check date)' do
+      last_event = Event.last
+      date = DateTime.now
+      put :update, params: {id: last_event.id, resources_collector: {date: date.to_s}}
+      last_event = Event.last
+      expect(last_event.date).to eq(date.to_s)
+    end
+
+  end
+
+  after :each do
+    event_params = nil
+  end
+
+end
