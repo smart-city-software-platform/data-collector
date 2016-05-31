35e30285153d9078a668c0a92b5c653f50b08df2
rodrigoadfaria <rodrigoadfaria@gmail.com>
2016-05-31 19:11:33 -0300

    Fixing the routes file
    Signed-off-by: rodrigoadfaria <rodrigoadfaria@gmail.com>

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+
+  get 'details/index'
+  get 'details/show'
+
+  resources :events, only: [:show, :index], defaults: {format: :json} do
+  	resources :details,
+  				defaults: {format: :json},
+  				only: [:show, :index]
+  end
+
+  resources :resources_collector, only: [:create, :update]
+
+end
