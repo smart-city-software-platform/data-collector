aac700c641d39a04c7779a68fa48e58d5ba0ce9b
rodrigosiqueira <rodrigosiqueiramelo@gmail.com>
2016-06-17 18:24:38 -0300

    Added update
    Signed-off-by: Arthur Del Esposte <arthurmde@gmail.com>
    Signed-off-by: João Brito  <joaojr.89@gmail.com

--- app/controllers/platform_resources_controller.rb
+++ app/controllers/platform_resources_controller.rb
@@ -1,5 +1,6 @@
 class PlatformResourcesController < ApplicationController
   skip_before_action :verify_authenticity_token
+  before_filter :find_platform_resource, only: [:edit]
 
   def create
     platform_resource = PlatformResource.new(platform_resource_params)
@@ -14,11 +15,39 @@ class PlatformResourcesController < ApplicationController
       end
       render json: {data: platform_resource}, status: 201
     else
-      render :json => { :error => "Internal Server Error" }, :status => 500
+      render json: { error: "Internal Server Error" }, status: 500
     end
   end
 
-  def edit
+  def update
+    begin
+      raise ActiveRecord::RecordNotFound unless @retrieved_resource
+      @retrieved_resource.update(platform_resource_params)
+      if capability_params
+        capabilities_hash = capability_params
+        capabilities_array = capability_hash[:capabilities]
+        target_to_remove = @retrieved_resource.capabilities
+                                .where("name not in (?)", capabilities_array)
+        # Removing old capabilities
+        @retrieved_resource.capabilities.delete(target_to_remove)
+
+        # Add new capabilities
+        if capabilities_array.kind_of? Array
+          capabilities_array.each do |capability_name|
+            capability = Capability.find_or_create_by(name: capability_name)
+            unless @retrieved_resource.capabilities.include?(capability)
+              @retrieved_resource.capabilities << capability
+            end
+          end
+        end
+        # TODO: Restart data collect
+      else
+        # TODO: Find right exception
+        raise ActiveRecord::RecordNotFound
+      end
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: "Not found" }, status: 404
+    end
   end
 
   private
@@ -29,6 +58,10 @@ class PlatformResourcesController < ApplicationController
 
     def capability_params
       params.require(:data).permit(capabilities: [])
+    end
+
+    def find_platform_resource
+      @retrieved_resource = PlatformResource.find_by_uuid(params[:uuid])
     end
 
 end
--- app/models/platform_resource_capability.rb
+++ app/models/platform_resource_capability.rb
@@ -3,4 +3,6 @@ class PlatformResourceCapability < ApplicationRecord
   belongs_to :capability
   belongs_to :platform_resource
 
+  validates_uniqueness_of :capability_id, scope: :platform_resource
+
 end
