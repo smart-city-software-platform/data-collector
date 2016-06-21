4f56ae383a3493caa1ba4cc850bb239eff805003
Joao Brito <joaojr.89@gmail.com>
2016-06-21 16:12:08 -0300

    Updated specs of capability and resource models
    Signed-off-by: Joao Brito <joaojr.89@gmail.com>

--- spec/models/capability_spec.rb
+++ spec/models/capability_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Capability, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+	let(:capability) {
+		FactoryGirl.build(:capability)
+	}
+
+	it 'has a valid factory' do
+		expect(capability).to be_valid
+	end
+
+	it 'has a name' do
+		expect(capability.name).not_to be_nil
+    expect(capability.name).not_to eq('')
+
+    expect(FactoryGirl.build(:capability , :name => '')).not_to be_valid
+    expect(FactoryGirl.build(:capability , :name => nil)).not_to be_valid
+	end
 end
