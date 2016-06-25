395b3ad127896039ca4be789ce60381ed3b38caa
Joao Brito <joaojr.89@gmail.com>
2016-06-25 13:13:46 -0300

    Include sensor values Hash to use in tests

--- spec/controllers/sensor_values_range_filter_spec.rb
+++ spec/controllers/sensor_values_range_filter_spec.rb
@@ -5,32 +5,107 @@ RSpec.describe SensorValuesController, type: :controller do
   context 'Verify request with filters' do
 
     before :each do
-      total = 4
-      status_opt = ['on', 'off', 'unknown', 'wtf']
-      list_of_capabilities = ['no', 'temperature', 'humidity', 'pressure']
-      @uuids = []
-
+      status_opt = 'on'
+      uuids_hash = {
+        "2de545ae-841a-4e4a-b961-a43bb324a2b9": {
+            'temperature': [
+              { 'value': '18.5',
+                'date': '2016-03-01 09:00:00'},
+              { 'value': '22.15',
+                'date': '2016-03-01 10:00:00'},
+              { 'value': '27',
+                'date': '2016-03-02 08:00:00'},
+              { 'value': '19.5',
+                'date': '2016-03-03 07:00:00'},
+            ],
+            'humidity': [
+              { 'value': '68',
+                'date': '2016-03-01 09:00:00'},
+              { 'value': '72',
+                'date': '2016-03-01 10:00:00'},
+              { 'value': '87',
+                'date': '2016-03-02 08:00:00'},
+              { 'value': '50',
+                'date': '2016-03-03 07:00:00'},
+            ]
+        },
+        "8fcbba32-ea98-4a84-9abf-c97c9c65c3c4": {},
+        "989e93f2-35e1-4a2b-b80a-4bf91030085c": {
+            'people': [
+              { 'value': '70',
+                'date': '2016-03-01 09:01:00'},
+              { 'value': '11',
+                'date': '2016-03-01 10:01:00'},
+              { 'value': '22',
+                'date': '2016-03-02 08:01:00'},
+              { 'value': '40',
+                'date': '2016-03-03 07:01:00'}
+            ],
+            'quality': [
+              { 'value': '1',
+                'date': '2016-03-01 09:30:00'},
+              { 'value': '2',
+                'date': '2016-03-01 10:30:00'},
+              { 'value': '7',
+                'date': '2016-03-02 08:30:00'},
+              { 'value': '5',
+                'date': '2016-03-03 07:30:00'}
+            ]
+        },
+        "a9f4d13b-1c10-474c-9754-a0a92adcc72d": {
+            'temperature': [
+              { 'value': '28.5',
+                'date': '2016-03-01 09:00:00'},
+              { 'value': '29.5',
+                'date': '2016-03-03 07:00:00'}
+            ],
+            'pressure': [
+              { 'value': '1038',
+                'date': '2016-03-01 09:00:00'},
+              { 'value': '1012',
+                'date': '2016-03-03 07:00:00'}
+            ],
+            'humidity': [
+              { 'value': '70',
+                'date': '2016-03-01 09:30:00'},
+              { 'value': '72',
+                'date': '2016-03-01 10:30:00'},
+              { 'value': '67',
+                'date': '2016-03-02 08:30:00'},
+              { 'value': '75',
+                'date': '2016-03-03 07:30:00'}
+            ],
+            'people': [
+              { 'value': '70',
+                'date': '2016-03-01 09:30:00'},
+              { 'value': '0',
+                'date': '2016-03-01 10:30:00'},
+              { 'value': '22',
+                'date': '2016-03-02 08:30:00'},
+              { 'value': '40',
+                'date': '2016-03-03 07:30:00'}
+            ]
+        }
+      }
       # Creating data on database
-      total.times do |index|
-        @uuids.push(SecureRandom.uuid)
-        uri = "/basic_resources/#{Faker::Number.between(1,10)}/components/" +
-              "#{Faker::Number.between(1,10)}/collect"
-
-        resource = PlatformResource.create!(uuid: @uuids[index],
+      uuids_hash.each do |uuid, capability_hash|
+        uri = "/basic_resources/1/components/" +
+              "#{Faker::Number.between(1, 10)}/collect"
+        resource = PlatformResource.create!(uuid: uuid,
                             uri: uri,
-                            status: status_opt[rand(status_opt.size)],
+                            status: status_opt,
                             collect_interval: Faker::Number.between(60, 1000))
-        total_cap = Faker::Number.between(1,3)
+
         # Create capabilities
-        total_cap.times do |index|
-          capability = Capability.find_or_create_by(name: list_of_capabilities[index])
+        capability_hash.each do |capability_name, values_list|
+          capability = Capability.find_or_create_by(name: capability_name)
           resource.capabilities << capability
 
-          2.times do |j|
+          values_list.each do |value_hash|
             SensorValue.create!(capability: capability,
                           platform_resource: resource,
-                          date: Faker::Time.between(DateTime.now - 1, DateTime.now),
-                          value: Faker::Number.decimal(2, 3))
+                          date: Time.parse(value_hash[:date]),
+                          value: value_hash[:value] )
           end
         end
       end
@@ -48,13 +123,13 @@ RSpec.describe SensorValuesController, type: :controller do
 
       it 'Correct list of capabilities for range with existing capability' do
         post 'resources_data', params: {sensor_value: 
-                                   {range: {pressure: {min: 0, max: 22}}}}
+                                   {range: {temperature: {min: 0, max: 100}}}}
         returned_json = JSON.parse(response.body)
         retrieved_resource = returned_json['resources']
-        
         retrieved_uuids = retrieved_resource.map(&Proc.new {|element|
                                                           element['uuid']} )
 
+        expect(retrieved_uuids.empty?).to be_falsy
         retrieved_uuids.each do |uuid|
           platform = PlatformResource.find_by_uuid(uuid)
           real_capabilities = platform.capabilities.pluck(:name)
@@ -63,7 +138,7 @@ RSpec.describe SensorValuesController, type: :controller do
           end.first['capabilities'].keys
 
           expect(real_capabilities).to include(*retrieved_capabilities)
-          expect(['pressure']).to include(*retrieved_capabilities)
+          expect(['temperature']).to match_array(retrieved_capabilities)
         end
       end
 
@@ -72,11 +147,49 @@ RSpec.describe SensorValuesController, type: :controller do
                                    {range: {wontfindnocap: {min: 0, max: 22}}}}
         returned_json = JSON.parse(response.body)
         retrieved_resource = returned_json['resources']
-        
         retrieved_uuids = retrieved_resource.map(&Proc.new {|element|
                                                           element['uuid']} )
 
         expect(retrieved_uuids.size).to eq(0)
+      end
+
+
+      it 'Empty list for conflicting multiple capabilities' do
+        post 'resources_data', params: {sensor_value: 
+                                   {range: {
+                                        temperature: {min: 0, max: 100},
+                                        quality: {min: 0, max: 100} }}}
+        returned_json = JSON.parse(response.body)
+        retrieved_resource = returned_json['resources']
+        retrieved_uuids = retrieved_resource.map(&Proc.new {|element|
+                                                          element['uuid']} )
+
+        expect(retrieved_uuids.size).to eq(0)
+      end
+
+      it 'Correct list of capabilities for range multiple capabilities' do
+        post 'resources_data', params: {sensor_value: 
+                                   {range: {
+                                        temperature: {min: 1, max: 101},
+                                        humidity: {min: 2, max: 102} }}}
+        returned_json = JSON.parse(response.body)
+        retrieved_resource = returned_json['resources']
+        retrieved_uuids = retrieved_resource.map(&Proc.new {|element|
+                                                          element['uuid']} )
+
+        # TODO: is retrieving empty because concatenation of ranges
+        # instead, the filter should use keyword "IN"
+        expect(retrieved_uuids.empty?).to be_falsy
+        retrieved_uuids.each do |uuid|
+          platform = PlatformResource.find_by_uuid(uuid)
+          real_capabilities = platform.capabilities.pluck(:name)
+          retrieved_capabilities = retrieved_resource.select do |element|
+            element['uuid'] == uuid
+          end.first['capabilities'].keys
+
+          expect(real_capabilities).to include(*retrieved_capabilities)
+          expect(['temperature', 'humidity']).to match_array(retrieved_capabilities)
+        end
       end
 
     end
