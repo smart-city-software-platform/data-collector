9b9a2c830e9c81b0db5ea6b96a062047a2a5f827
rodrigosiqueira <rodrigosiqueiramelo@gmail.com>
2016-06-26 20:09:19 -0300

    Drafts

--- lib/worker-manager/worker_supervisor.rb
+++ lib/worker-manager/worker_supervisor.rb
@@ -4,45 +4,64 @@ require 'singleton'
 # lot of workers collecting data from thousands of resources because of this
 # we have to control basic things related to it.
 class WorkerSupervisor
-  include Singleton
 
-  # @map_worker_byId
+	include Singleton
 
-  def initialize
-    # @map_worker_by_id = {}
-  end
+	@@resource_id_status = {}
 
-  # Based on the new resource, we have to request periodically data
-  # to /basic_resources/:id/components and handle it. This method, build the
-  # URI and perform the required work.
-  def create_worker(resource_id)
-    # PygmentsWorker.perform_async(@snippet.id)
-    register_worker(resource_id)
-  end
+	INACTIVE ||= 0
+	ACTIVE ||= 1
+	UPDATED ||= 2
 
-  # Sometimes, we need to start and stopped worker. It is possible to do it
-  # when some method provides the Id from resource.
-  # @param resource_id Target resource to start collect.
-  def start_worker(resource_id)
-    # TODO
-  end
+	def set_resource_collector_status(resource_id, status)
+		@@resource_id_status[resource_id] = status
+	end
 
-  # Find a resource with worker currently running and stopped it.
-  # @param resource_id Target to stop.
-  def stop_worker(resource_id)
-    # TODO
-  end
+	def resource_status(resource_id)
+puts '=' * 30
+puts resource_id
+puts @@resource_id_status
+puts @@resource_id_status[resource_id]
+puts @@resource_id_status[resource_id] || INACTIVE
+puts '=' * 30
+		if @@resource_id_status[resource_id].nil?
+			INACTIVE
+		else
+			@@resource_id_status[resource_id]
+		end
+	end
 
-  # Verify if has an worker running, if it has destroy each one. If no
-  # workers are currently running, go to database and start to collect
-  # all data.
-  def spawn_workers
-    # TODO
-  end
+	# Based on the new resource, we have to request periodically data
+	# to /basic_resources/:id/components and handle it. This method, build the
+	# URI and perform the required work.
+	def create_worker(resource_id)
+		# PygmentsWorker.perform_async(@snippet.id)
+		register_worker(resource_id)
+	end
 
-  # Just take a target id associate it with a PID from worker in a hash.
-  # @param resource_id
-  def register_worker(resource_id)
-    # TODO
-  end
+	# Sometimes, we need to start and stopped worker. It is possible to do it
+	# when some method provides the Id from resource.
+	# @param resource_id Target resource to start collect.
+	def start_worker(resource_id)
+		# TODO
+	end
+
+	# Find a resource with worker currently running and stopped it.
+	# @param resource_id Target to stop.
+	def stop_worker(resource_id)
+		# TODO
+	end
+
+	# Verify if has an worker running, if it has destroy each one. If no
+	# workers are currently running, go to database and start to collect
+	# all data.
+	def spawn_workers
+		# TODO
+	end
+
+	# Just take a target id associate it with a PID from worker in a hash.
+	# @param resource_id
+	def register_worker(resource_id)
+		# TODO
+	end
 end
