ec6a94b7fe34f33f3ddbbbeb46d197fcc94f6036
rodrigosiqueira <rodrigosiqueiramelo@gmail.com>
2016-06-28 11:11:50 -0300

    Handle unique constraints on capability
    Signed-off-by: João Brito <joaojr.89@gmail.com>

--- app/controllers/platform_resources_controller.rb
+++ app/controllers/platform_resources_controller.rb
@@ -63,7 +63,11 @@ class PlatformResourcesController < ApplicationController
   def assotiate_capability_with_resource(capabilities, resource)
     if capabilities.kind_of? Array
       capabilities.each do |capability_name|
-        capability = Capability.find_or_create_by(name: capability_name)
+        begin
+          capability = Capability.find_or_create_by(name: capability_name)
+        rescue ActiveRecord::RecordNotUnique
+          capability = Capability.find_by_name(capability_name)
+        end
         unless resource.capabilities.include?(capability)
           resource.capabilities << capability
         end
