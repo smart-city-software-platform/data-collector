2296783007d141ede3505b6f28eb2f0285d4d7cf
Arthur Del Esposte <arthurmde@gmail.com>
2016-11-05 12:29:16 -0200

    Add sidekiq workers to collect data and manager resources

---
+++ app/workers/create_resources.rb
@@ -0,0 +1,75 @@
+require 'bunny'
+require 'rubygems'
+require 'json'
+
+class CreateResources
+  include Sidekiq::Worker
+
+  TOPIC = 'resource_create'
+  QUEUE = 'data_collection_resource_create'
+
+  def initialize
+    @conn = Bunny.new(hostname: SERVICES_CONFIG['services']['rabbitmq'])
+    @conn.start
+    @channel = @conn.create_channel
+    @topic = @channel.topic(TOPIC)
+    @queue = @channel.queue(QUEUE)
+  end
+
+  def perform
+    @queue.bind(@topic, routing_key: '#.sensor.#')
+
+    begin
+      @queue.subscribe(:block => true) do |delivery_info, properties, body|
+        routing_keys = delivery_info.routing_key.split('.')
+        json = JSON.parse(body)
+        resource_attributes = json.slice(
+          'uri',
+          'uuid',
+          'status',
+          'collect_interval',
+          'created_at',
+          'updated_at'
+        )
+
+        create_resource(resource_attributes, json)
+      end
+    rescue Exception => e
+      @channel.close
+      @conn.close
+      LOGGER.error("ResourcesCreate: channel closed - #{e.message}")
+    end
+  end
+  
+  private
+  
+  def create_resource(resource_attributes, json)
+    begin
+      resource = PlatformResource.new(resource_attributes)
+      resource.save!
+      assotiate_capability_with_resource(json['capabilities'], resource)
+    rescue ActiveRecord::RecordNotUnique => err
+      LOGGER.error("ResourcesCreate: Error when tried to create resource. #{err}")
+    rescue ActiveRecord::RecordInvalid => invalid
+      LOGGER.error("ResourcesCreate: Attempt to store resource: #{invalid.record.errors}")
+    end
+  end
+
+  def assotiate_capability_with_resource(capabilities, resource)
+    if capabilities.kind_of? Array
+      capabilities.each do |capability_name|
+        # Thread safe
+        begin
+          capability = Capability.find_or_create_by(name: capability_name)
+        rescue ActiveRecord::RecordNotUnique => err
+          LOGGER.info("ResourcesCreate: Attempt to create duplicated capability. #{err}")
+          capability = Capability.find_by_name(capability_name)
+        end
+
+        unless resource.capabilities.include?(capability)
+          resource.capabilities << capability
+        end
+      end
+    end
+  end
+end
---
+++ app/workers/update_resources.rb
@@ -0,0 +1,90 @@
+require 'bunny'
+require 'rubygems'
+require 'json'
+
+class UpdateResources
+  include Sidekiq::Worker
+
+  TOPIC = 'resource_update'
+  QUEUE = 'data_collection_resource_update'
+
+  def initialize
+    @conn = Bunny.new(hostname: SERVICES_CONFIG['services']['rabbitmq'])
+    @conn.start
+    @channel = @conn.create_channel
+    @topic = @channel.topic(TOPIC)
+    @queue = @channel.queue(QUEUE)
+  end
+
+  def perform
+    @queue.bind(@topic, routing_key: '#.sensor.#')
+
+    begin
+      @queue.subscribe(:block => true) do |delivery_info, properties, body|
+        routing_keys = delivery_info.routing_key.split('.')
+        json = JSON.parse(body)
+        resource_attributes = json.slice(
+          'uri',
+          'uuid',
+          'status',
+          'collect_interval',
+          'created_at',
+          'updated_at'
+        )
+
+      update_resource(resource_attributes, json)
+      end
+    rescue Exception => e
+      @channel.close
+      @conn.close
+      LOGGER.error("UpdateResources: channel closed - #{e.message}")
+    end
+  end
+  
+  private
+  
+  def update_resource(resource_attributes, json)
+    begin
+      resource = PlatformResource.find_by_uuid(json['uuid'])
+      raise ActiveRecord::RecordNotFound unless resource
+      resource.update!(resource_attributes)
+
+      capabilities = json['capabilities']
+      remove_needed_capabilities(capabilities, resource)
+
+      assotiate_capability_with_resource(json['capabilities'], resource)
+    rescue ActiveRecord::RecordNotFound => err
+      LOGGER.error("ResourcesUpdate: Could not find resource with uuid #{json['uuid']}. #{err}")
+    rescue ActiveRecord::RecordInvalid => invalid
+      LOGGER.error("ResourcesUpdate: Attempt to store resource: #{invalid.record.errors}")
+    end
+  end
+
+  def assotiate_capability_with_resource(capabilities, resource)
+    if capabilities.kind_of? Array
+      capabilities.each do |capability_name|
+        # Thread safe
+        begin
+          capability = Capability.find_or_create_by(name: capability_name)
+        rescue ActiveRecord::RecordNotUnique => err
+          LOGGER.info("UpdateResources: Attempt to create duplicated capability. #{err}")
+          capability = Capability.find_by_name(capability_name)
+        end
+
+        unless resource.capabilities.include?(capability)
+          resource.capabilities << capability
+        end
+      end
+    end
+  end
+
+  def remove_needed_capabilities(capabilities, resource)
+    # If no capabilities are present inside hash, just remove all
+    if capabilities.nil?
+      resource.capabilities.delete_all
+    else
+      rm = resource.capabilities.where('name not in (?)', capabilities)
+      resource.capabilities.delete(rm)
+    end
+  end
+end
