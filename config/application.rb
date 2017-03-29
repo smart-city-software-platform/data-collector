9a66d3c76d4b05db3905ea868342e275e645a661
Arthur Del Esposte <arthurmde@gmail.com>
2017-03-29 15:57:58 -0300

    Add basic structure to mongodb

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,14 @@
 require_relative 'boot'
 
-require 'rails/all'
+require "rails"
+require "active_model/railtie"
+require "active_job/railtie"
+require "action_controller/railtie"
+require "action_cable/engine"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
 require 'net/http'
 
 # Require the gems listed in Gemfile, including any gems
